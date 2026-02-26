a47173802cabc1b7795cbab16f4d52224368fab4
VForiel <vincent.foriel@gmail.com>
2026-02-26 16:08:16 +0100

    Fix site base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://vincent.foriel.xyz',
+    site: 'https://vforiel.github.io',
+    base: '/New-Website',
     build: {
         assets: '_assets'
     },
